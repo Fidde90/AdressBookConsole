901f4c5de801d04158d06484435ade18888af8a4
Fredrik <fredrik.t.bengtsson@hotmail.com>
2023-12-06 15:29:42 +0100

    Added folders and created the contact interface and contact class.

---
+++ AdressBookConsole/Interfaces/IContact.cs
@@ -0,0 +1,19 @@
+﻿
+namespace AdressBookConsole.Interfaces;
+
+public interface IContact
+{
+    public string FirstName { get; set; }
+
+    public string LastName { get; set; }
+
+    public int PhoneNumber { get; set; }
+
+    public string Email { get; set; }
+
+    public string Street { get; set; }
+
+    public int PostalCode { get; set; }
+
+    public string City { get; set; }
+}
---
+++ AdressBookConsole/Models/Contact.cs
@@ -0,0 +1,15 @@
+﻿
+using AdressBookConsole.Interfaces;
+
+namespace AdressBookConsole.Models;
+
+public class Contact : IContact
+{
+    public string FirstName { get; set; } = null!;
+    public string LastName {get; set; } = null!;
+    public int PhoneNumber {get; set;}
+    public string Email {get; set; } = null!;
+    public string Street {get; set; } = null!;
+    public int PostalCode {get; set;}
+    public string City {get; set; } = null!;
+}
