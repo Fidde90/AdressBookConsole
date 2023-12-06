2f73475d29ecc0a8677d9424b19ecc837845dbed
Fredrik <fredrik.t.bengtsson@hotmail.com>
2023-12-06 15:37:23 +0100

    Added constructor to the contact class.

--- AdressBookConsole/Models/Contact.cs
+++ AdressBookConsole/Models/Contact.cs
@@ -7,17 +7,32 @@ public class Contact : IContact
 {
     public string FirstName { get; set; } = null!;
 
-    public string LastName {get; set; } = null!;
+    public string LastName { get; set; } = null!;
 
-    public int PhoneNumber {get; set;}
+    public int PhoneNumber { get; set; }
 
-    public string Email {get; set; } = null!;
+    public string Email { get; set; } = null!;
 
-    public string Street {get; set; } = null!;
+    public string Street { get; set; } = null!;
 
-    public int PostalCode {get; set;}
+    public int PostalCode { get; set; }
 
-    public string City {get; set; } = null!;
+    public string City { get; set; } = null!;
 
     public string Country { get; set; } = null!;
+
+
+    public Contact() { }
+
+    public Contact(string firstName, string lastName, int phoneNumber, string email, string street, int postalCode, string city, string country)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        PhoneNumber = phoneNumber;
+        Email = email;
+        Street = street;
+        PostalCode = postalCode;
+        City = city;
+        Country = country;
+    }
 }
